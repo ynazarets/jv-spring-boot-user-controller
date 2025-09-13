75ef3fd99b5899ecfd330c7875ecaa1390e43540
Yehor Nazarets <nazarets1111@gmail.com>
2025-09-13 18:03:10 +0200

    created user controller

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,25 @@
 package mate.academy.springboot.web.controller;
 
-public class UserController {
+import java.util.List;
+import mate.academy.springboot.web.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public List<User> getAll() {
+        return List.of(
+                new User(1L, "bob@i.ua"),
+                new User(2L, "alice@i.ua"));
+    }
+
+    @PostMapping
+    public String save(@RequestBody User user) {
+        return "User created. Id: %s, email: %s".formatted(user.id(), user.email());
+    }
 }
